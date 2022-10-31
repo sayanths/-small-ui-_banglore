ebadb567045299e9f46805bf454ea4c553eab474
sayanth A <sayanthsayu619@gmail.com>
2022-10-31 21:25:53 +0530

    completed

--- lib/app/home_page/view/home_page.dart
+++ lib/app/home_page/view/home_page.dart
@@ -1,5 +1,5 @@
+import 'package:another_ui_banglore/app/home_page/view/widgets/allcustomwidgets.dart';
 import 'package:another_ui_banglore/app/home_page/view/widgets/custom_container.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,156 +13,12 @@ class HomePage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
-                          "Helllo Rakib",
-                          style: TextStyle(color: Colors.grey, fontSize: 20),
-                        ),
-                        CircleAvatar(
-                          backgroundColor: Colors.red,
-                          radius: 25,
-                          child: Icon(
-                            CupertinoIcons.search,
-                            size: 30,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Find your course",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ))
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                height: 180,
-                width: 380,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    begin: Alignment.bottomRight,
-                    colors: [
-                      Color.fromARGB(255, 75, 97, 222),
-                      Color.fromARGB(255, 83, 175, 251),
-                    ],
-                  ),
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Color.fromARGB(255, 166, 178, 246),
-                        offset: Offset(-0, 10),
-                        blurRadius: 10,
-                        spreadRadius: 2),
-                  ],
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Column(
-                                children: [
-                                  const Text(
-                                    "60% off",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 35,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  const Text(
-                                    "Feb 14-Mar 20",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  SizedBox(
-                                    width: 120,
-                                    child: ElevatedButton(
-                                      onPressed: () {},
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor: Colors.orange,
-                                          shape: const StadiumBorder()),
-                                      child: const Text("Join Now"),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              Container(
-                                height: 180,
-                                width: 200,
-                                decoration: const BoxDecoration(
-                                  color: Colors.white10,
-                                  borderRadius: BorderRadius.only(
-                                      topRight: Radius.circular(30),
-                                      bottomRight: Radius.circular(30),
-                                      bottomLeft: Radius.circular(80),
-                                      topLeft: Radius.circular(80)),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Image.asset(
-                                    "assets/bag.png",
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      "Subject",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "see all",
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
+              height20,
+              const MainHeadingsCustom(),
+              height20,
+              const CustomMainContainer(),
+              height20,
+              const CustomSubjectAndSeelAll(),
               Wrap(
                 children: [
                   CustomSubjectContainer(
@@ -176,7 +32,7 @@ class HomePage extends StatelessWidget {
                       subTitle: '\$14/month',
                       lastTile: 'To study chemistry'),
                   CustomSubjectContainer(
-                    height: 60,
+                      height: 60,
                       image: 'assets/sat2.png',
                       mainTtitle: 'Biology',
                       subTitle: '\$16/month',
---
+++ lib/app/home_page/view/widgets/allcustomwidgets.dart
@@ -0,0 +1,174 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const Widget height20 = SizedBox(
+  height: 20,
+);
+
+class MainHeadingsCustom extends StatelessWidget {
+  const MainHeadingsCustom({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                "Helllo Rakib",
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 25,
+                child: Icon(
+                  CupertinoIcons.search,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+          const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Find your course",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMainContainer extends StatelessWidget {
+  const CustomMainContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      width: 380,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 2, 30, 190),
+            Color.fromARGB(255, 83, 175, 251),
+          ],
+        ),
+        boxShadow: const [
+          BoxShadow(
+              color: Color.fromARGB(255, 166, 178, 246),
+              offset: Offset(-0, 10),
+              blurRadius: 10,
+              spreadRadius: 2),
+        ],
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          "60% off",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const Text(
+                          "Feb 14-Mar 20",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 120,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.orange,
+                                shape: const StadiumBorder()),
+                            child: const Text("Join Now"),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      height: 180,
+                      width: 200,
+                      decoration: const BoxDecoration(
+                        color: Colors.white10,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(30),
+                            bottomRight: Radius.circular(30),
+                            bottomLeft: Radius.circular(80),
+                            topLeft: Radius.circular(80)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          "assets/bag.png",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomSubjectAndSeelAll extends StatelessWidget {
+  const CustomSubjectAndSeelAll({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Text(
+            "Subject",
+            style: TextStyle(
+                color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "see all",
+            style: TextStyle(
+                color: Colors.grey, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
